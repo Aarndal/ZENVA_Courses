724559ed544687c0290676270686b03f642cee85
Aarndal <aarndal@duck.com>
2026-02-10 19:57:32 +0100

    2D JumpnRun minigame

---
+++ Assets/_ZENVA_Courses/2D_JumpnRun/Scripts/CoinSpawner.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour, ISpawner
+{
+
+
+    public void StartSpawning()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopSpawning()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+}
+
